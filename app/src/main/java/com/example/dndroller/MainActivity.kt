e246b23386c2f2c51d0dff00a4e5932cd4d130cc
Alexis Huerta <alehuer90@gmail.com>
2019-08-04 13:16:06 -0700

    added critical rolls toast

--- app/src/main/java/com/example/dndroller/MainActivity.kt
+++ app/src/main/java/com/example/dndroller/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +15,43 @@ class MainActivity : AppCompatActivity() {
 
         //R stands for Resources
         findViewById<Button>(R.id.button).setOnClickListener{
-            Log.i("Alexis", "Button is clicked") //search the message by looking up the tag
-            findViewById<TextView>(R.id.textView).text = "You Rolled a :"
+
+            val lowerBound = 1
+            val upperBound = 20
+            //get randomNumber and change the view
+            val randomNumber = null
+
+            //if randomNumber is the winning number, then show a toast message
+//            val winningNumber = findViewById<EditText>(R.id.editText).text.toString()
+//            if(winningNumber == randomNumber){
+//                Toast.makeText(this,"You Won!", Toast.LENGTH_LONG).show()
+//            }
+
+            for(i in 5..10)
+            {
+
+                val randomNumber = getRandomNumber(lowerBound,upperBound).toString()
+
+                findViewById<TextView>(R.id.textView).text = "You Rolled a : " + randomNumber + " !"
+                Log.i("Alexis", "Button is clicked" + randomNumber) //search the message by looking up the tag
+                Thread.sleep(1000)
+            }
+
+            if(lowerBound.toString() == randomNumber){
+                Toast.makeText(this,"Critical Miss!", Toast.LENGTH_LONG).show()
+            }
+
+            if(upperBound.toString() == randomNumber){
+                Toast.makeText(this,"Critical Hit!", Toast.LENGTH_LONG).show()
+            }
+
+
+//            var randomNumber = getRandomNumber(1,20)
+//            buttonText = "You Rolled a : " + randomNumber + " !"
         }
+    }
+
+    private fun getRandomNumber(lowerBound: Int, upperBound: Int): Int {
+        return (lowerBound..upperBound).random()
     }
 }
