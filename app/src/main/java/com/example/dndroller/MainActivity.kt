7145b86e0b2137b5d30f5e6652233923690c7f41
Alexis Huerta <alehuer90@gmail.com>
2019-08-04 11:36:11 -0700

    can change text using a button

--- app/src/main/java/com/example/dndroller/MainActivity.kt
+++ app/src/main/java/com/example/dndroller/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.dndroller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) //This is how the view is shown on this Activity
+
+        //R stands for Resources
+        findViewById<Button>(R.id.button).setOnClickListener{
+            Log.i("Alexis", "Button is clicked") //search the message by looking up the tag
+            findViewById<TextView>(R.id.textView).text = "You Rolled a :"
+        }
     }
 }
